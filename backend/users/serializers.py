66c76e69237c0adf7af11839994f6eaac4723c9d
Divyanshu <divyanshubargali@gmail.com>
2020-09-06 09:24:48 +0530

    Added Readme.md

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,7 +5,7 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
   class Meta:
     model = Profile
-    fields = ("user"
+    fields = ("user",
         "first_name",
         "last_name",
         "about",
